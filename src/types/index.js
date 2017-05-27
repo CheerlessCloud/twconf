157f38098747e28e982efd61b8487f682e3704b9
EnRoute <nnsceg@gmail.com>
2017-05-27 12:09:08 +0300

    feat: Add index file for types

---
+++ src/types/index.js
@@ -0,0 +1,7 @@
+export { default as TwConfFieldType } from './base';
+
+export { default as String } from './string';
+export { default as Number } from './number';
+export { default as Int } from './int';
+export { default as Float } from './float';
+export { default as IPAddress } from './ip-address';
