30406f89056cb29459e14eae4f9c1acd0d22bd29
EnRoute <nnsceg@gmail.com>
2017-05-27 11:57:53 +0300

    feat(Add base FieldType):

---
+++ src/types/base.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {object} TwConfFieldType
+ */
+export default class TwConfFieldType {
+  /**
+   * Array of validators for this type
+   * @type {function.<boolean>[]}
+   */
+  validators = [];
+}
