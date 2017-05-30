e33ca421c1daf9e0b7fc63334b8812b20ec3d23b
EnRoute <nnsceg@gmail.com>
2017-05-30 07:28:14 +0300

    feat: Add boolean type and tests

---
+++ src/types/boolean.js
@@ -0,0 +1,24 @@
+/**
+ * Created by enroute on 30.05.17.
+ */
+import ConfigFieldBaseType from './base';
+
+class BooleanType extends ConfigFieldBaseType {
+  constructor() {
+    super();
+
+    this.validators.push((val) => {
+      if (typeof val === 'boolean') {
+        return true;
+      }
+
+      return typeof val === 'string' &&
+        ['true', 'false', '1', '0'].includes(val.toLowerCase());
+    });
+
+    this.transformators.post.push(val => ['true', '1'].includes(val.toLowerCase()));
+  }
+}
+
+export default BooleanType;
+module.exports = BooleanType;
---
+++ src/types/boolean.test.js
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+/* globals describe, it, before, after, beforeEach, afterEach */
+
+import { expect } from 'chai';
+import BooleanType from './boolean';
+import BaseType from './base';
+
+describe('boolean type', () => {
+  it('check base type', () => {
+    expect(new BooleanType())
+      .to.be.an.instanceof(BaseType, 'All types must extend base type');
+  });
+
+  it('check validators', () => {
+    const booleanType = new BooleanType();
+
+    booleanType.validators.map(fn => expect(fn('true')).to.be.true);
+    booleanType.validators.map(fn => expect(fn('TRUE')).to.be.true);
+    booleanType.validators.map(fn => expect(fn('1')).to.be.true);
+    booleanType.validators.map(fn => expect(fn('false')).to.be.true);
+    booleanType.validators.map(fn => expect(fn('FALSE')).to.be.true);
+    booleanType.validators.map(fn => expect(fn('0')).to.be.true);
+
+    booleanType.validators.map(fn => expect(fn('t')).to.be.false);
+    booleanType.validators.map(fn => expect(fn('fls')).to.be.false);
+  });
+
+  it('check transformators', () => {
+    const booleanType = new BooleanType();
+
+    booleanType.transformators.post.map(fn => expect(fn('true')).to.be.true);
+    booleanType.transformators.post.map(fn => expect(fn('TRUE')).to.be.true);
+    booleanType.transformators.post.map(fn => expect(fn('1')).to.be.true);
+    booleanType.transformators.post.map(fn => expect(fn('false')).to.be.false);
+    booleanType.transformators.post.map(fn => expect(fn('FALSE')).to.be.false);
+    booleanType.transformators.post.map(fn => expect(fn('0')).to.be.false);
+  });
+});
