fe329b4b85b9e8e792000f8bb58aa0f2e7005535
EnRoute <nnsceg@gmail.com>
2017-08-13 14:53:06 +0300

    tets(types): Added tests for float type

---
+++ src/types/float.test.js
@@ -0,0 +1,22 @@
+/* eslint no-unused-expressions: "off" */
+/* eslint-env mocha */
+/* globals describe, it, before, after, beforeEach, afterEach */
+
+import { expect } from 'chai';
+import FloatType from './float';
+import BaseType from './base';
+
+describe('float type', () => {
+  it('check base type', () => {
+    expect(new FloatType())
+      .to.be.an.instanceof(BaseType, 'All types must extend base type');
+  });
+
+  it('precision validator', () => {
+    const type = new FloatType({ precision: 3 });
+
+    expect(type.validators[3](282.33)).to.be.false;
+    expect(type.validators[3](282.333)).to.be.true;
+    expect(type.validators[3](282.333333)).to.be.true;
+  });
+});
