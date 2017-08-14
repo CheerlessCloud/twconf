f9c3b14800eda68e59ad3d343ed9c21ab3460173
EnRoute <nnsceg@gmail.com>
2017-08-14 05:38:06 +0300

    feat(utils/to-map): Add utils/to-map converter.

---
+++ src/utils/to-map.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line consistent-return
+function toMap(collection) {
+  if (collection instanceof Map) {
+    return collection;
+  } else if (collection instanceof Array) {
+    return new Map(collection);
+  } else if (typeof collection === 'object') {
+    return new Map(Object.entries(collection));
+  }
+
+  throw new Error('No collection');
+}
+
+export default toMap;
