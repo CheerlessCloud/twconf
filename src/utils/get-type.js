1ff1ac52e0063c7e61ab9f95697580db5c876de4
EnRoute <nnsceg@gmail.com>
2017-08-14 07:36:54 +0300

    feat(utils/get-type): Add utils/get-type function, who will resolve type of field.

---
+++ src/utils/get-type.js
@@ -0,0 +1,45 @@
+import EError from 'eerror';
+import Types from './../types';
+
+const types = new Map(
+  Object.entries(Types)
+    .map(([name, value]) =>
+      ([
+        name.toLowerCase().replace(/type/ig, ''),
+        value,
+      ]),
+    ),
+);
+
+function getType(config = {}) {
+  if (config.type instanceof Types.ConfigFieldBaseType) {
+    return config.type;
+  } else if (typeof config.type === 'string') {
+    const CurrentType = types.get(config.type);
+
+    if (!CurrentType) {
+      throw new EError('Unknown type', { typeName: config.type });
+    }
+
+    return new CurrentType(config);
+  } else if (typeof config.type === 'object') {
+    const typename =
+         config.type.name
+      || config.type.type
+      || config.type.timename
+      || config.type.timeName;
+    const CurrentType = types.get(typename.toLowerCase());
+
+    if (!CurrentType) {
+      throw new EError('Unknown type', { typeName: typename });
+    }
+
+    return new CurrentType({ ...config, ...config.type });
+  }
+
+  throw new EError('Type of field must be string, object or object extend from ConfigFieldBaseType', {
+    fieldName: config.name,
+  });
+}
+
+export default getType;
