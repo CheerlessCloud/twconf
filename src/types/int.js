28314b06df803219263b6c1f48817b33dee0960f
EnRoute <nnsceg@gmail.com>
2017-08-14 04:41:45 +0300

    feat(types): Change signature of IntType constructor.

--- src/types/int.js
+++ src/types/int.js
@@ -3,8 +3,8 @@ import NumberType from './number';
 const { round, abs } = Math;
 
 class IntType extends NumberType {
-  constructor(min, max) {
-    super(min, max);
+  constructor({ min, max } = {}) {
+    super({ min, max });
 
     this.validators.push(val => (abs(val) - round(abs(val))) === 0);
   }
